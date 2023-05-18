e6ad7037a0dcb869fb67266fb1cfb83c057f276d
ASip1 <98257155+ASip1@users.noreply.github.com>
2023-05-18 18:55:51 -0400

    Lab1

---
+++ Lab1/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My partner's name is Aaron Siple. His preferred pronouns are he/him.");
+        System.out.println("He is taking this class while working towards an associates degree in software engineering at OCC.");
+        System.out.println("When he is not in class, he is usually working at his job within the automotive industry.");
+        System.out.println("Fun Fact: Aaron used to play the drums and percussion in a professional band.");
+    }
+}
+
